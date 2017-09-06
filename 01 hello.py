b10db2a485fd95598423cd2ff6d36f6d8dfbc41e
marshallkarre <31518975+marshallkarre@users.noreply.github.com>
2017-09-06 12:07:29 -0400

    Changes capitalization in "Call of Duty", Adds concerns, excitements. stackoverflow #, and github url

--- 01 hello.py
+++ 01 hello.py
@@ -5,4 +5,8 @@ import sys
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
 print('My name is Karre')
-print('My favorie game is call of duty')
+print('My favorie game is Call of Duty')
+print('The concerns I have with this class is catching up')
+print('I am exited about the general idea of being able to code a working program')
+print('I am stackoverflow user number 8544876')
+print('My GitHub URL is https://github.com/marshallkarre')
